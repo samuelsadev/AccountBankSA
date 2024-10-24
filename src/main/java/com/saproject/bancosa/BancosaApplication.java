b7b904b70e0b610f60962f3eeb52dada306e3b7a
samuelsadev <samuelsa.dev@gmail.com>
2024-10-24 16:40:32 -0300

    Iniciando Porjeto BancoSA

---
+++ src/main/java/com/saproject/bancosa/BancosaApplication.java
@@ -0,0 +1,13 @@
+package com.saproject.bancosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saproject/bancosa/BancosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saproject.bancosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
