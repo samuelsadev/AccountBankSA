1d1862cddd37a67541fc38111dc2312b39a3cc51
samuelsadev <samuelsa.dev@gmail.com>
2024-12-06 02:20:25 -0300

    Atualizando busca por email ou cpf

--- src/main/java/com/saproject/bancosa/repository/UsuarioRepository.java
+++ src/main/java/com/saproject/bancosa/repository/UsuarioRepository.java
@@ -9,12 +9,6 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    Optional<Usuario> findByEmailOrCpf(String email, String cpf);
-
-    @Query("SELECT u FROM Usuario u WHERE u.email = :emailOrCpf OR u.cpf = :emailOrCpf")
-    Optional<Usuario> findByEmailOrCpf(@Param("emailOrCpf") String emailOrCpf);
-
-
     Optional<Usuario> findByEmail(String email);
 
     Optional<Usuario> findByCpf(String cpf);
